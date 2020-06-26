68df16bca006ac8ef487cd6aec409ef30df43e27
unknown <djaredchase@gmail.com>
2020-06-26 15:14:40 -0400

    tier1 commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+dataType = (a) => {
+    return typeof a;
+}
+
+product = (a,b) => {
+    return a*b;
+} 
+
+sumArray = (a) => {
+    let sum = 0;
+    let i;
+    for(i=0; i<a.length; i++){
+        sum+=a[i];
+    }
+    return sum;
+}
+
+const e = Math.pow(2,3);
+console.log(e);
+
+exponent = (n) => {
+    const sN = n.toString();
+    //console.log('this is my log ' + typeof sN);
+    const nArr = sN.split("");
+    let length = nArr.length;
+    let numArr = nArr.map(i => parseInt(i));
+    let i;
+    let sum = 0;
+    for(i = 0; i<numArr.length; i++) {
+        sum+=Math.pow(numArr[i], length);
+    }
+    console.log(sum);
+    return sum==n;
+}
+
+console.log(exponent(100));
+
+/*
+let a = '12345';
+let b = a.split("");
+console.log(b);
+let c = b.map(i => parseInt(i));
+console.log(c);
+*/
